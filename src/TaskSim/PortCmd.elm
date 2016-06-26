60806ec31df35da9492c7eae668d9468c7e06448
Yosuke Torii <jinjorweb@gmail.com>
2016-06-27 04:55:52 +0900

    PortCmd.map

--- src/TaskSim/PortCmd.elm
+++ src/TaskSim/PortCmd.elm
@@ -1,8 +1,19 @@
 module TaskSim.PortCmd exposing (PortCmd, none)
 
 import TaskSim.EffectManager as EM exposing (EffectManager)
+import TaskSim.PortTask as PortTask
 
 type alias PortCmd msg = EM.PortCmd msg
 
+
 none : PortCmd msg
 none = EM.Simple Cmd.none
+
+
+map : (a -> b) -> PortCmd a -> PortCmd b
+map f portCmd =
+  case portCmd of
+    EM.Simple cmd ->
+      EM.Simple (Cmd.map f cmd)
+    EM.Callback json toTask ->
+      EM.Callback json (PortTask.mapError f << PortTask.map f << toTask)
